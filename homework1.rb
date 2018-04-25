8f0f30ce31bf978be9fdbf61e06c121c0a7b1699
aliaksandrzaitsau <35954552+aliaksandrzaitsau@users.noreply.github.com>
2018-04-25 19:01:50 +0300

    Update homework1.rb

--- homework1.rb
+++ homework1.rb
@@ -5,16 +5,14 @@
 #2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
 #10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290'
 
-input.each_line do |line|
-  list = []
-   if (line.downcase['error'])
-     #puts 'There is some ERRORS'
-     list.push(line)
-   else
-     #puts 'SUCCESSFULL'
-     list.push(line.split('[')[1].split(']')[0] + " FROM:" + line.split[0] + " TO:" + line.upcase.split('POST')[1].split('HTTP')[0])
-   end
-   puts list
+def task_1(input)
+  list = ''
+  input.each_line do |line|
+     if (line.downcase.include? 'error')
+       list << line[0..-2]
+     end
+  end
+  list
 end
 
 ############TASK2##############
@@ -27,20 +25,33 @@ end
 #2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
 #2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event')
 
-require 'time'
-list = []
-input.each_line do |line|
-  list.push Time.parse(line) if line.include? 'Calling'
+def task_2(input)
+  task2_list = []
+  input.each_line do |lines|
+    if (lines.match(/(?:\d{1,3}\.){3}\d{1,3} - - \[(.*?)\] "POST \/.* HTTP\/.*/))
+    task2_list << lines.split('[')[1].split(']')[0] + " FROM: " + lines.split[0] + " TO:" + lines.upcase.split('POST')[1].split('HTTP')[0][0..-2] if (lines.downcase.match("^(?!.*error).*$"))
+    end
+  end
+  task2_list
 end
-  puts list[1] - list[0]
 
 ############TASK3#############
 
+def task_3(input)
+  require 'time'
+  list = []
+  input.each_line do |line|
+    list.push Time.parse(line) if line.include? 'Calling'
+  end
+    list[1] - list[0]
+end
+
 #input = 'вещественные числа, типа 3.14 рассматриваются как 3 числа'
 
-sum = 0
-regex = /[[:digit:]]+/
-input.scan(regex)  { |num|
-    sum += num.to_i
-  }
-puts "#{sum}"
+def task_4(input)
+  sum = 0
+  input.each_char do |letter|
+    sum += letter.to_i if letter.to_i.is_a? Integer
+  end
+  sum
+end
